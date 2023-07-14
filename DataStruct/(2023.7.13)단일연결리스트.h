e02baa456cb81f4b09ad1dcf2216cbf5b38e9fe4
Dokky <107966608+dokky41@users.noreply.github.com>
2023-07-14 21:49:34 +0900

    (2023.7.14)양방향 연결 리스트

---
+++ DataStruct/(2023.7.13)단일연결리스트.h
@@ -0,0 +1,122 @@
+#include <iostream>
+
+struct NODE
+{
+	int data;
+	NODE* next;
+};
+
+class LinkedList
+{
+private:
+	NODE* head;
+	NODE* tail;
+
+	int size;
+
+public:
+	LinkedList()
+	{
+		head = nullptr;
+		tail = nullptr;
+
+		size = 0;
+	}
+
+	void Push_Front(int data)
+	{
+		// 1. 새로운 노드 생성
+		NODE* newNode = new NODE;
+		// 2. 새로운 노드의 data값을 저장합니다.
+		newNode->data = data;
+		// 3. 새로운 노드의 next 값은 NULL로 저장합니다
+		newNode->next = NULL;
+
+		// if(head==nullptr)
+		if (head == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			newNode->next = head;
+			head = newNode;
+		}
+
+		size++;
+	}
+
+	void Push_Back(int data)
+	{
+		// 1. 새로운 노드 생성
+		NODE* newNode = new NODE;
+		// 2. 새로운 노드의 data값을 저장합니다.
+		newNode->data = data;
+		// 3. 새로운 노드의 next 값은 NULL로 저장합니다
+		newNode->next = NULL;
+
+		// if(head==nullptr)
+		if (tail == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	void Insert(NODE* prevNode, int data)
+	{
+
+	}
+
+
+	int Size()
+	{
+		return size;
+	}
+
+	void PrintList() {
+
+		NODE* node = head;
+
+		std::cout << "연결리스트 목록 = ( ";
+
+		while (node != nullptr) {
+
+			std::cout << node->data << " ";
+			node = node->next;
+		}
+		std::cout << ")" << std::endl;
+
+	}
+
+
+	~LinkedList()
+	{
+	
+	}
+};
+
+int main()
+{
+	LinkedList list;
+
+	list.Push_Back(20);
+	list.Push_Front(10);
+
+	std::cout << list.Size() << std::endl;
+
+	//list.PrintList();
+
+	std::cout << std::endl;
+
+
+	return 0;
+}
--- DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct.cpp
@@ -1,123 +1,125 @@
 ﻿#include <iostream>
 
-// 1. 노드 만들기
+
 struct NODE
 {
 	int data;
 	NODE* next;
+	NODE* prev;
+};
 
+template <typename T>
+class DoubleLinkedList
+{
+private:
+	NODE* head;
+	NODE* tail;
+
+public:
+	DoubleLinkedList()
+	{
+		head = nullptr;
+		tail = nullptr;
+	
+	}
+
+	void Push_Front(T data)
+	{
+		NODE* newNode = new NODE;
+		newNode->data = data;
+		newNode->next = NULL;
+		newNode->prev = NULL;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			newNode->next = head;
+			head->prev = newNode;
+			head = newNode;
+		}
+	
+	}
+
+	void Push_Back(T data)
+	{
+		NODE* newNode = new NODE;
+		newNode->data = data;
+		newNode->next = NULL;
+		newNode->prev = NULL;
+
+		if (tail == nullptr)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			newNode->prev = tail;
+			tail->next = newNode;
+			tail = newNode;
+		}
+	
+	}
+
+	void pop_Front()
+	{
+		if (head == nullptr)
+		{
+			return;
+		}
+		else {
+			NODE* removePtr = head;
+
+			head = head->next;
+
+			head->prev = nullptr;
+
+			delete removePtr;
+		}
+	}
+
+	void pop_Back()
+	{
+		if (head == nullptr)
+		{
+			return;
+		}
+		else 
+		{
+			NODE* removePtr = tail;
+
+			tail = tail->prev;
+
+			tail->next = nullptr;
+
+			delete removePtr;
+		}
+	}
+
+
+	~DoubleLinkedList()
+	{
+		while (head != nullptr)
+		{
+			NODE* node = head;
+			head = node->next;
+			head->prev = nullptr;
+			delete node;
+		}
+	}
 
 };
 
-void AddNode(NODE* target, int data)
-{
-	// 1.노드 생성
-	NODE* newNode = new NODE;
-
-	// 2.새로운 노드의 data값을 저장
-	newNode->data = data;
-
-	// 3.새로운 노드의 next 포인터에 이전 노드의 다음 주소를 저장합니다. (NewNode->next = target의 next)
-	newNode->next = target->next;
-
-	// 4. 이전 노드의 다음 주소에 새로운 노드의 주소를 저장합니다.   (target->next = 새로운 노드)
-	target->next = newNode;
-}
-
-
-void ShowNode(NODE* targetPtr)
-{
-	// 순회용 포인터
-	//1. 순회용 포인터에 dummy의 주소를 저장
-	NODE* currentPtr = targetPtr;
-
-	//2. 순회용 포인터가 nullptr이 아니라면 반복하도록 설정한다.
-	//3. 순회용 포인터를 계속 갱신한다. (= 순회용 포인터에 next 포인터를 저장한다)
-	//4. 현재 순회용 포인터로 값을 출력한다.
-	while (currentPtr != nullptr)
-	{
-		currentPtr = currentPtr->next;
-
-		if (currentPtr != nullptr)
-		{
-			std::cout << currentPtr->data << std::endl;
-		}
-	}
-
-}
-
-
 int main()
 {
-
-#pragma region 단일 연결 리스트
-
-	// 단일 연결 리스트
-	/*
-	//          [dummy 노드]
-	NODE * dummy = new NODE;
-	//          [node1 노드]
-	NODE * node1 = new NODE;
-	//          [node2 노드]
-	NODE * node2 = new NODE;
+	DoubleLinkedList<int> list;
+	
 
 
 
-	// 1. dummy의 next 포인터 <- node1의 시작 주소를 저장
-	dummy->next = node1;
-
-	// 2. node1의 data변수 10으로 초기화
-	node1->data = 10;
-
-	// 3. node1의 next 포인터 <- node2의 시작 주소를 저장
-	node1->next = node2;
-
-	// 4. node2의 data변수 20으로 초기화
-	node2->data = 20;
-
-	// 5. node2의 next 포인터 <- nullptr 저장
-	node2->next = nullptr;
-
-
-	// 순회용 포인터
-	//1. 순회용 포인터에 dummy의 주소를 저장
-	NODE* currentPtr =  dummy;
-
-	//2. 순회용 포인터가 nullptr이 아니라면 반복하도록 설정한다.
-	//3. 순회용 포인터를 계속 갱신한다. (= 순회용 포인터에 next 포인터를 저장한다)
-	//4. 현재 순회용 포인터로 값을 출력한다.
-	while (currentPtr != nullptr)
-	{
-		currentPtr = currentPtr->next;
-
-		if(currentPtr != nullptr)
-		{
-			std::cout << currentPtr->data << std::endl;
-		}
-	}
-
-	delete dummy;
-	delete node1;
-	delete node2;
-	*/
-
-	// 단일 연결 리스트 (함수)
-	NODE* dummy = new NODE;
-
-	dummy->next = nullptr;
-
-	AddNode(dummy, 10);
-	AddNode(dummy, 20);
-	AddNode(dummy, 30);
-
-	ShowNode(dummy);
-
-	delete(dummy);
-
-#pragma endregion
-
-
-
-
-
+	return 0;
 }
