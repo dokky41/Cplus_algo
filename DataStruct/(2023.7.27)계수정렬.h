8ce45ed29cd7000ef4dff89a6c3df128de7748ea
Dokky <107966608+dokky41@users.noreply.github.com>
2023-07-28 21:45:47 +0900

    (2023.7.28) 퀵 정렬

---
+++ DataStruct/(2023.7.27)계수정렬.h
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#define SIZE 10
+
+int main()
+{
+
+#pragma region 계수 정렬
+	// 데이터의 값을 직접 비교하지 않고, 단순하게 각 숫자가 
+	// 몇 개 있는지 갯수를 세어 저장한 다음 정렬하는 알고리즘입니다.
+
+	//int numbers[SIZE] = { 1,3,3,4,5,5,5,1,2,2 };
+	//int cntArray[5] = { 0, };
+	//
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	cntArray[numbers[i]-1]++;
+	//}
+	//
+	//for (int j = 0; j < 5; j++)
+	//{
+	//	std::cout << cntArray[j] << " ";
+	//}
+
+
+#pragma endregion
+
+
+
+	return 0;
+}
